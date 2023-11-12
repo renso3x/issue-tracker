ebbb0527995f06550588b510891b1e23e28d3323
Romeo Enso <romeo.enso@connexionltd.com>
2023-11-13 09:37:55 +1100

    Fix caching issues

--- app/api/issues/route.tsx
+++ app/api/issues/route.tsx
@@ -1,12 +1,12 @@
 import { NextRequest, NextResponse } from "next/server";
 import prisma from '../../../prisma/client'
-import { createIssueSchema } from "./createIssueSchema";
+import { issueSchema } from "./createIssueSchema";
 
 
 export async function POST(request: NextRequest) {
     const body = await request.json()
 
-    const validation = createIssueSchema.safeParse(body)
+    const validation = issueSchema.safeParse(body);
 
     if (!validation.success)
         return NextResponse.json(validation.error.errors, { status: 400 })
