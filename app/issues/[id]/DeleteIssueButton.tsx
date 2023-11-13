90bdb7424d62fbfab9f90ad97b43600ed79d7b55
Romeo Enso <romeo.enso@connexionltd.com>
2023-11-14 09:42:00 +1100

    Remove duplicate pages

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -4,15 +4,19 @@ import { AlertDialog, Button, Flex } from "@radix-ui/themes";
 import axios from "axios";
 import { useRouter } from "next/navigation";
 import { Fragment, useState } from "react";
+import { Spinner } from "../../components";
 
 const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
   const router = useRouter();
   const [error, setError] = useState(false);
+  const [isDeleting, setDeleting] = useState(false);
 
   const deleteIssue = async () => {
+    setDeleting(true);
     try {
+      setDeleting(false);
       await axios.delete(`/api/issues/${issueId}`);
-      router.push("/issues");
+      router.push("/issues/list");
       router.refresh();
     } catch (error) {
       setError(true);
@@ -23,7 +27,9 @@ const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
     <Fragment>
       <AlertDialog.Root>
         <AlertDialog.Trigger>
-          <Button color="red">Delete Issue</Button>
+          <Button color="red" disabled={isDeleting}>
+            Delete Issue {isDeleting && <Spinner />}
+          </Button>
         </AlertDialog.Trigger>
         <AlertDialog.Content>
           <AlertDialog.Title>Delete Confirmation</AlertDialog.Title>
