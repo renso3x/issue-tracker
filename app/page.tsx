a3f83974068069850549e308bbb238691878fc89
Romeo Enso <romeo.enso@connexionltd.com>
2023-11-23 08:39:32 +1100

    build layout of the pagination component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
+import Pagination from "./components/Pagination";
 
 export default function Home() {
   return (
-   <h1>Dashboard</h1>
+    <Pagination itemCount={100} pageSize={20} currentPage={5} />
   )
 }
